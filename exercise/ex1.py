6b6e3d5da9ab7009cef2fff8919c6950f7f0fdcb
SoneLiu <358372734@qq.com>
2019-03-30 14:55:30 +0800

    add ex1

---
+++ exercise/ex1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: Marte
+# @Date:   2019-03-30 14:47:49
+# @Last Modified by:   Marte
+# @Last Modified time: 2019-03-30 14:48:40
+print "hello world"
+print "hello liuyunpeng"
