b8f8543c1d1a465059d32ea58156ed482040e4f8
SoneLiu <358372734@qq.com>
2019-03-30 15:01:37 +0800

    changes some

--- exercise/ex1.py
+++ exercise/ex1.py
@@ -2,6 +2,6 @@
 # @Author: Marte
 # @Date:   2019-03-30 14:47:49
 # @Last Modified by:   Marte
-# @Last Modified time: 2019-03-30 14:48:40
-print "hello world"
-print "hello liuyunpeng"
+# @Last Modified time: 2019-03-30 15:00:31
+print("hello world")
+print("hello liuyunpeng")
